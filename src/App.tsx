c8a9534da61625655a7aa6f490d8cb5067574f77
coldmater <coldmater@gmail.com>
2019-06-02 23:52:55 +0900

    [UPDATE] 파일 업로드 및, 업로드 파일 인식

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logo from './logo.svg';
+import { useDropzone } from 'react-dropzone';
 import './App.css';
 
 const App: React.FC = () => {
+
+  const onDrop = useCallback( acceptedFiles => {
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log('file reading was aborted');
+    reader.onerror = () => console.log('file reading has failed');
+    reader.onload = () => {
+      const binaryStr:string = reader.result as string;
+      console.log(reader);
+      console.log(binaryStr.split('\n'));
+      console.log(binaryStr);
+    }
+
+    acceptedFiles.forEach((file:any) => reader.readAsBinaryString(file));
+
+  }, []);
+
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({onDrop});
+  
+  const files = acceptedFiles.map((file:any) => (
+    <li key={file.path}>
+      {file.path}
+    </li>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          변위 센서 사폭 분석
+        </h1>
       </header>
+      <div className="controller-container">
+        <section className="controller">
+          <h2>지관 row 데이터 업로드</h2>
+          <div {...getRootProps({className: 'dropzone'})}>
+            <input {...getInputProps()}/>
+            <p>{ files || "지관 파일을 drag 또는 선택해주세요."}</p>
+          </div>
+        </section>
+        <section className="controller">
+          <h2>지관 row 데이터 업로드</h2>
+          <div {...getRootProps({className: 'dropzone'})}>
+            <input {...getInputProps()}/>
+            <p>지관 파일을 drag 또는 선택해주세요.</p>
+          </div>
+        </section>
+      </div>
+
+      <section className="result-viewer">
+      </section>
     </div>
   );
 }
