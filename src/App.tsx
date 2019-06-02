cd620c27d8baf103ef44907f5d1f559f72aade9f
coldmater <coldmater@gmail.com>
2019-06-03 03:50:04 +0900

    [UPDATE] gap, limit 값을 기반으로 filtering 및 그래프 표출

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,68 @@ import G_Controller from './controller';
 import Chart from './chart';
 
 const App: React.FC = () => {
-  const [jigwanData, setJigwandata] = useState<number[]>([]);
-  const [gaesumData, setGaesumdata] = useState<number[]>([]);
-  const [jigwanLimit, setJigwanLimit] = useState<number>(2);
-  const [jigwanGap, setJigwanGap] = useState<number>(1);
-  const [gaesumLimit, setGaesumLimit] = useState<number>(2);
-  const [gaesumGap, setGaesumGap] = useState<number>(1);
+	const [jigwanData, setJigwandata] = useState<number[]>([]);
+	const [gaesumData, setGaesumdata] = useState<number[]>([]);
 
-  const setJigwanData = (data: any) => {
-    setJigwandata(data);
-  }
+	const [jigwanLimit, setJigwanLimit] = useState<number>(2);
+	const [jigwanGap, setJigwanGap] = useState<number>(1);
+	const [gaesumLimit, setGaesumLimit] = useState<number>(2);
+	const [gaesumGap, setGaesumGap] = useState<number>(1);
 
-  const setGaesumData = (data: any) => {
-    setGaesumdata(data)
-  }
+	const [filteredJigwanData, setFilteredJigwandata] = useState<number[]>([]);
+	const [filteredGaesumData, setFilteredGaesumdata] = useState<number[]>([]);
 
-  const inputFormContainer = (name: string, limit:number, gap: number, setLimit: any, setGap: any) => (
+	const setJigwanData = (data: any) => {
+		setJigwandata(data);
+	}
+
+	const setGaesumData = (data: any) => {
+		setGaesumdata(data)
+	}
+
+	const setFilteredJigwanData = (data: any) => {
+		setFilteredJigwandata(data);
+	}
+
+	const setFilteredGaesumData = (data: any) => {
+		setFilteredGaesumdata(data)
+	}
+
+	const jigwanRefresh = () => {
+		const filteredData = dataFilter(jigwanData, jigwanLimit, jigwanGap);
+		setFilteredJigwandata(filteredData);
+	}
+
+	const gaesumRefresh = () => {
+		const filteredData = dataFilter(gaesumData, gaesumLimit, gaesumGap);
+		setFilteredGaesumdata(filteredData);
+	}
+
+	const dataFilter = (data: number[], limit: number, gap: number) => {
+		const filteredData = data.filter((number, index) => {
+			if(index === 0 ){
+				return true;
+			} else {
+				const difference = Math.abs(number-data[index-1]);
+				return !(number < limit || difference > gap);
+			}
+		})
+		
+		return filteredData;
+	}
+
+  const inputFormContainer = (name: string, limit:number, gap: number, setLimit: any, setGap: any, refresh: any) => (
       <div className="input-form">
         <div className="input-row">
 			<label className="form-label">하한값&nbsp;</label>
-			<input value={limit} onChange={(e)=>setLimit(e.target.value)}></input>
+			<input type={'number'} step={0.1} min={0} value={limit} onChange={(e)=>{setLimit(e.target.value); refresh()}}></input>
 		</div>
 		<div className="input-row">
 			<label className="form-label">gap값&nbsp;</label>
-			<input value={gap} onChange={(e)=>setGap(e.target.value)}></input>
+			<input type={'number'} step={0.1} min={0} value={gap} onChange={(e)=>{setGap(e.target.value); refresh()}}></input>
 		</div>
 		<div className="input-row">
-			<button>{name} filter</button>
+			<button onClick={refresh}>{name} filter</button>
 		</div>
 		</div>
   )
@@ -46,12 +81,12 @@ const App: React.FC = () => {
 			</h1>
 		</header>
 		<div className="controller-container">
-			<J_Controller title={"지관 row 데이터 업로드"} setData = {setJigwanData}/>
-			<G_Controller title={"개섬 row 데이터 업로드"} setData = {setGaesumData}/>
+			<J_Controller title={"지관 row 데이터 업로드"} setData = {setJigwanData} limit={jigwanLimit} gap={jigwanGap}/>
+			<G_Controller title={"개섬 row 데이터 업로드"} setData = {setGaesumData} limit={gaesumLimit} gap={gaesumGap}/>
 		</div>
 		<div className="input-form-container">
-			{inputFormContainer("지관", jigwanLimit, jigwanGap, setJigwanLimit, setJigwanGap)}
-			{inputFormContainer("개섬", gaesumLimit, gaesumGap, setGaesumLimit, setGaesumGap)}
+			{inputFormContainer("지관", jigwanLimit, jigwanGap, setJigwanLimit, setJigwanGap, jigwanRefresh)}
+			{inputFormContainer("개섬", gaesumLimit, gaesumGap, setGaesumLimit, setGaesumGap, gaesumRefresh)}
 		</div>
 
 		<hr></hr>
@@ -61,6 +96,14 @@ const App: React.FC = () => {
 			</div>
 			<div className="result">
 				<Chart data={gaesumData}/>
+			</div>
+		</section>
+		<section className="result-container">
+			<div className="result">
+				<Chart data={filteredJigwanData}/>
+			</div>
+			<div className="result">
+				<Chart data={filteredGaesumData}/>
 			</div>
 		</section>
     </div>
